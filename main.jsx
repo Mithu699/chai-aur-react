e282a435751cd76f82d89ce5d8518082dbffb0e2
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-11 21:28:27 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,32 +1,10 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-
-function MyApp() {
-    return (
-        <div>
-            <h1>
-                custom app
-            </h1>
-        </div>
-    )
-}
-
-const anotherElement = (
-    <a href="http://google.com" target='_blank'>visit google</a>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-const anotherUser = "chai or code"
-const reactElement = React.createElement( 
-    'a',
-    { href: 'https://google.com', target: '_blank' },
-    'click me to visit google',
-    anotherUser 
-)
-
-createRoot(document.getElementById('root')).
-    render(
-
-        reactElement
-    )
